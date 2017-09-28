bfbd51a833508796426a783d6ddc9dbf65c9a41e
Merb <michael.erbs@gmail.com>
2017-09-27 23:23:43 -0700

    added new content, and mobile styles fix

--- app/Ctrls/mainCtrl.js
+++ app/Ctrls/mainCtrl.js
@@ -9,26 +9,33 @@ ewdApp.controller('mainCtrl', ['$scope', function($scope){
 
   $scope.myWorks = [
     {
-      title: 'tied with strings',
-      tagLine: 'E-commerce and main site for compnay',
-      img: 'content/img/tiedwithstrings.png',
-      url: 'http://tiedwithstrings.com',
-      info: 'Still in process, e-commerce site',
-      features: ['Angular.js', 'Search+filters', 'Add to shopping bag', 'Purchase using Infusionsoft']
-    }, {
-      title: 'Unturned Ids',
-      tagLine: 'Search and copy ids of game assets',
-      img: 'content/img/unturned-ids.png',
-      url: 'http://unturned.erbwebdev.com/',
-      info: 'Search and copy ids of game assets for a game called Unturned. Super simple and fun. This site is for those that want to know the game id numbers for spawning in items',
-      features: ['Angular.js', 'infinite scroll', 'Search+filters', 'Build a custom list']
+      title: 'Key Codes',
+      tagLine: 'Fun personal project',
+      img: 'content/img/keycode.png',
+      url: 'http://keycode.erbwebdev.com/',
+      info: 'I saw a few other examples of this and thought it a fun experiment. It reads keyboard events and dynamically displays the keyCode and name of the key from browser\'s DOM event object.',
+      features: ['vanilla JS', 'keyboard event handling']
     }, {
       title: 'Mrmroofing',
-      tagLine: 'One page site for company',
+      tagLine: 'One page site for client',
       img: 'content/img/mrmroofing.png',
       url: 'http://mrmroofingllc.com/',
-      info: 'One page site for company. Using just classic HTML CSS and Javascript, nothing too fancy',
-      features: ['Smooth scroll to page sections', 'Client Work']
+      info: 'One page site for a client. Using just classic HTML CSS and Javascript, nothing too fancy',
+      features: ['Smooth scroll to page sections', 'Client Work', 'Mobile Responsive design']
+    }, {
+      title: 'tied with strings',
+      tagLine: 'E-commerce site',
+      img: 'content/img/tiedwithstrings.png',
+      url: 'http://tiedwithstrings.erbwebdev.com',
+      info: 'A personal project and first bigger project with angular.js. Learned a lot about javascript and angular.js. Static site with data is powered by static JSON file.',
+      features: ['Angular.js', 'Search+filters', 'Add to shopping bag', 'Purchase using Infusionsoft']
+    }, {
+      title: 'Erb Web Dev',
+      tagLine: 'One page site for myself',
+      img: 'content/img/ewd.png',
+      url: 'http://erbwebdev.com/',
+      info: 'Hero images created by yours truly. A website to house my work.',
+      features: ['angular.js', 'Page changing animations', 'Mobile Responsive design']
     }
   ];
 
@@ -48,11 +55,11 @@ ewdApp.controller('mainCtrl', ['$scope', function($scope){
     }, {
       name: 'JavaScript',
       img: 'Unofficial_JavaScript_logo.png',
-      notes: '80%'
+      notes: '95%'
     }, {
       name: 'Vue.js',
       img: 'vuejs-logo.png',
-      notes: '85%'
+      notes: '95%'
     }, {
       name: 'Angular.js',
       img: 'angular-js.png',
@@ -62,21 +69,21 @@ ewdApp.controller('mainCtrl', ['$scope', function($scope){
       img: 'nodejs-new-pantone-black.png',
       notes: '12%'
     }, {
-      name: 'PHP',
-      img: 'PHP-logo.svg.png',
-      notes: '10%'
-    }, {
       name: 'git',
       img: 'Git-Icon-1788C.png',
-      notes: '60%'
+      notes: '80%'
+    }, {
+      name: 'gulp',
+      img: 'gulp.png',
+      notes: '80%'
     }, {
       name: 'Adobe Photoshop',
       img: 'Adobe_Photoshop_CS5_icon.png',
-      notes: '16%'
+      notes: '26%'
     }, {
-      name: 'Adobe Illistrator',
+      name: 'Adobe Illustrator',
       img: 'Adobe_Illustrator_CS5_icon.png',
-      notes: '33%'
+      notes: '53%'
     }
   ]
 
--- app/Ctrls/workCtrl.js
+++ app/Ctrls/workCtrl.js
@@ -4,26 +4,33 @@ ewdApp.controller('workCtrl', ['$scope', function($scope){
 
   $scope.myWorks = [
     {
-      title: 'tied with strings',
-      tagLine: 'E-commerce and main site for compnay',
-      img: 'content/img/tiedwithstrings.png',
-      url: 'http://tiedwithstrings.com',
-      info: 'Still in process, e-commerce site',
-      features: ['Angular.js', 'Search+filters', 'Add to shopping bag', 'Purchase using Infusionsoft']
-    }, {
-      title: 'Unturned Ids',
-      tagLine: 'Search and copy ids of game assets',
-      img: 'content/img/unturned-ids.png',
-      url: 'http://unturned.erbwebdev.com/',
-      info: 'Search and copy ids of game assets for a game called Unturned. Super simple and fun. This site is for those that want to know the game id numbers for spawning in items',
-      features: ['Angular.js', 'infinite scroll', 'Search+filters', 'Build a custom list']
+      title: 'Key Codes',
+      tagLine: 'Fun personal project',
+      img: 'content/img/keycode.png',
+      url: 'http://keycode.erbwebdev.com/',
+      info: 'I saw a few other examples of this and thought it a fun experiment. It reads keyboard events and dynamically displays the keyCode and name of the key from browser\'s DOM event object.',
+      features: ['vanilla JS', 'keyboard event handling']
     }, {
       title: 'Mrmroofing',
-      tagLine: 'One page site for company',
+      tagLine: 'One page site for client',
       img: 'content/img/mrmroofing.png',
       url: 'http://mrmroofingllc.com/',
-      info: 'One page site for company. Using just classic HTML CSS and Javascript, nothing too fancy',
-      features: ['Smooth scroll to page sections', 'Client Work']
+      info: 'One page site for a client. Using just classic HTML CSS and Javascript, nothing too fancy',
+      features: ['Smooth scroll to page sections', 'Client Work', 'Mobile Responsive design']
+    }, {
+      title: 'tied with strings',
+      tagLine: 'E-commerce site',
+      img: 'content/img/tiedwithstrings.png',
+      url: 'http://tiedwithstrings.erbwebdev.com',
+      info: 'A personal project and first bigger project with angular.js. Learned a lot about javascript and angular.js. Static site with data is powered by static JSON file.',
+      features: ['Angular.js', 'Search+filters', 'Add to shopping bag', 'Purchase using Infusionsoft']
+    }, {
+      title: 'Erb Web Dev',
+      tagLine: 'One page site for myself',
+      img: 'content/img/ewd.png',
+      url: 'http://erbwebdev.com/',
+      info: 'Hero images created by yours truly. A website to house my work.',
+      features: ['angular.js', 'Page changing animations', 'Mobile Responsive design']
     }
   ];
 
